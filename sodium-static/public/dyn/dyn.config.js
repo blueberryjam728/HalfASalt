9c2f39deeb7009193240f5a7d20b69afeacf01b9
blueberryjam728 <gwaynevwa@gmail.com>
2024-03-05 12:42:58 -0600

    Try Again

--- sodium-static/public/dyn/dyn.config.js
+++ sodium-static/public/dyn/dyn.config.js
@@ -5,7 +5,7 @@ self.__dynamic$config = {
   logLevel: 3,
   bare: {
     version: 3,
-    path: "https://server2.flow-works.me/bare/"
+    path: "http://berrybare.us.to"
   },
   tab: {
     title: 'Service',
